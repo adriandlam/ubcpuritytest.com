16c61fbff8045f47c764cf305f918d164cd23dbe
Adrian Lam <adrianlamdev@gmail.com>
2025-03-28 22:18:55 -0700

    feat: finish general

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -24,11 +14,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<Script
+				defer
+				src="https://cloud.umami.is/script.js"
+				data-website-id="0bfe87b1-8a99-4ec0-a23d-23a4256ffa56"
+			/>
+			<body className="antialiased">{children}</body>
 		</html>
 	);
 }
